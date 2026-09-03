3e00f1b64219c50883f62d34acb79a085c3630fb
Alexander Scatov <scatovalexander@yandex.ru>
2026-09-03 09:32:13 +0300

    ошибка IllegalStateException: Body parameters cannot be used with form parameters

--- core/interaction-api/src/main/java/ru/yandex/practicum/feigns/event/EventPrivateFeign.java
+++ core/interaction-api/src/main/java/ru/yandex/practicum/feigns/event/EventPrivateFeign.java
@@ -1,8 +1,5 @@
 package ru.yandex.practicum.feigns.event;
 
-import jakarta.validation.Valid;
-import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.Positive;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -20,27 +17,28 @@ public interface EventPrivateFeign {
 
     @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
     @ResponseStatus(HttpStatus.CREATED)
-    EventFullDto addEvent(
-            @PathVariable @Positive Long userId,
-            @Valid @RequestBody NewEventDto newEventDto);
+    public EventFullDto addEvent(
+            @PathVariable Long userId,
+            @RequestBody NewEventDto newEventDto
+    );
 
     @PatchMapping(value = "/{eventId}", consumes = MediaType.APPLICATION_JSON_VALUE)
     @ResponseStatus(HttpStatus.OK)
     EventFullDto updateEvent(
-            @PathVariable @Positive Long userId,
-            @PathVariable @Positive Long eventId,
-            @Valid @RequestBody UpdateEventUserRequest updateEventUserRequest);
+            @PathVariable Long userId,
+            @PathVariable Long eventId,
+            @RequestBody UpdateEventUserRequest updateEventUserRequest);
 
     @GetMapping
     @ResponseStatus(HttpStatus.OK)
-    List<EventFullDto> getUserEvents(
-            @PathVariable @Positive Long userId,
-            @RequestParam(defaultValue = "0") @Min(0) Integer from,
-            @RequestParam(defaultValue = "10") @Positive Integer size);
+    public List<EventFullDto> getUserEvents(
+            @PathVariable Long userId,
+            @RequestParam(defaultValue = "0") Integer from,
+            @RequestParam(defaultValue = "10") Integer size);
 
     @GetMapping("/moderation")
     @ResponseStatus(HttpStatus.OK)
-    List<EventFullDto> getUserModerationHistory(
+    public List<EventFullDto> getUserModerationHistory(
             @PathVariable Long userId,
             @RequestParam(defaultValue = "0") Integer from,
             @RequestParam(defaultValue = "10") Integer size
@@ -48,17 +46,17 @@ public interface EventPrivateFeign {
 
     @GetMapping("/{eventId}")
     @ResponseStatus(HttpStatus.OK)
-    EventFullDto getUserEventById(
-            @PathVariable @Positive Long userId,
-            @PathVariable @Positive Long eventId);
+    public EventFullDto getUserEventById(
+            @PathVariable Long userId,
+            @PathVariable Long eventId);
 
     @GetMapping("/by-id-status-time")
     @ResponseStatus(HttpStatus.OK)
     List<EventFullDto> findEventsBySubscriberIdAndStatusAndTimeAfter(
-            @PathVariable @Positive Long userId,
+            @PathVariable Long userId,
             @RequestParam EventState state,
             @RequestParam LocalDateTime now,
-            @RequestParam(defaultValue = "0") @Min(0) int from,
-            @RequestParam(defaultValue = "10") @Positive int size
+            @RequestParam(defaultValue = "0") int from,
+            @RequestParam(defaultValue = "10") int size
     );
 }
